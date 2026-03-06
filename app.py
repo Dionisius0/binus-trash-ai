5242d348f9a4bf51fc9957f17214f355256eb6e0
Dionisius0 <dionisius.fredi@binus.ac.id>
2026-03-06 18:22:30 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from PIL import Image
 import numpy as np
 import os
 import gdown
+from pillow_heif import register_heif_opener # Ini adalah 'penerjemah' foto iPhone
+
+# Mengaktifkan fungsi agar sistem bisa membaca format .HEIC milik iPhone
+register_heif_opener()
 
 # --- BAGIAN RAHASIA: MENJEMPUT OTAK AI DARI DRIVE ---
 @st.cache_resource
@@ -20,7 +24,7 @@ def download_dan_muat_model():
 
 model = download_dan_muat_model()
 
-# --- TAMPILAN MEWAH ---
+# --- TAMPILAN MEWAH (MODERN DARK GREEN) ---
 st.set_page_config(page_title="Binus Trash AI", layout="wide")
 st.markdown("""
     <style>
@@ -43,8 +47,9 @@ kiri, kanan = st.columns([1.5, 1])
 with kiri:
     st.markdown('<div class="glass-card">', unsafe_allow_html=True)
     st.subheader("📁 LANGKAH 1: UNGGAH")
-    # Izin masuk untuk format foto sudah ditambah di bawah ini
-    foto = st.file_uploader("Pilih gambar sampah", type=["jpg", "png", "jpeg", "webp", "jfif"])
+    # Izin masuk sudah ditambah untuk format HEIC (iPhone)
+    foto = st.file_uploader("Pilih gambar sampah (JPG, PNG, HEIC)", type=["jpg", "png", "jpeg", "webp", "jfif", "heic"])
+    
     if foto:
         img = Image.open(foto).convert('RGB')
         st.image(img, use_container_width=True)
@@ -54,7 +59,9 @@ with kiri:
 with kanan:
     st.markdown('<div class="glass-card">', unsafe_allow_html=True)
     st.subheader("HASIL KLASIFIKASI")
+    
     if foto and 'tombol' in locals() and tombol:
+        # Proses AI menebak
         img_res = img.resize((180, 180))
         arr = tf.keras.utils.img_to_array(img_res)
         arr = tf.expand_dims(arr, 0)
@@ -63,8 +70,10 @@ with kanan:
         
         if hasil == 0:
             st.success("### 🍃 HASIL: ORGANIK")
+            st.write("Ini adalah sampah alami yang bisa membusuk.")
         else:
             st.info("### ♻️ HASIL: ANORGANIK")
+            st.write("Ini adalah sampah buatan yang sulit hancur.")
     else:
         st.write("Menunggu gambar diunggah...")
     st.markdown('</div>', unsafe_allow_html=True)
